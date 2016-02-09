232dc80496c8015658cc294aa29e78ba118fbcbd
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-02-09 11:32:55 +0100

    Initial commit

--- config/index.js
+++ config/index.js
@@ -3,9 +3,13 @@
 var config = {
   ARCHIVE_DIRECTORY_PATH: process.env.ARCHIVE_DIRECTORY_PATH || 'test/data/archive',
   DONE_DIRECTORY_PATH: process.env.DONE_DIRECTORY_PATH || 'test/data/done',
-  ERROR_DIRECTORY_PATH: process.env.DONE_DIRECTORY_PATH || 'test/data/error',
+  ERROR_DIRECTORY_PATH: process.env.DONE_DIRECTORY_PATH || 'test/data/errors',
   JOB_DIRECTORY_PATH: process.env.JOB_DIRECTORY_PATH || 'test/data/jobs',
-  JWT_KEY: process.env.JWT_KEY || 'Louie Louie, oh no, I got to go'
+  JWT_KEY: process.env.JWT_KEY || 'Louie Louie, oh no, I got to go',
+  DSF_URL: process.env.DSF_URL || 'http://ws-test.infotorg.no/xml/ErgoGroup/DetSentraleFolkeregister1_4/2015-08-10/DetSentraleFolkeregister1_4.wsdl',
+  DSF_NAMESPACE: process.env.DSF_NAMESPACE || 'http://ws.infotorg.no/xml/Admin/Brukersesjon/2006-07-07/Brukersesjon.xsd',
+  DSF_USERNAME: process.env.DSF_USERNAME || 'MrSmith',
+  DSF_PASSWORD: process.env.DSF_PASSWORD || 'MrSmithsPassword'
 }
 
 module.exports = config
--- index.js
+++ index.js
@@ -4,7 +4,9 @@ function roy (item, callback) {
   var miss = require('mississippi')
   var streamifier = require('streamifier')
   var getNextJob = require('./lib/get-next-job')
+  var lookupDsf = require('./lib/lookup-dsf')
   var saveJobDone = require('./lib/save-job-done')
+  var saveJobError = require('./lib/save-job-error')
   var cleanupJob = require('./lib/cleanup-job')
   var sendStatusMessage = require('./lib/send-status-message')
   var setupItem = require('./lib/setup-item')
@@ -22,7 +24,9 @@ function roy (item, callback) {
     start,
     getNextJob,
     setupItem,
+    lookupDsf,
     saveJobDone,
+    saveJobError,
     cleanupJob,
     sendStatusMessage,
     finished
