166ec821b2da83720951f864b2d7babfc9267018
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-02-10 13:00:36 +0100

    Implements SvarUT and p360

--- config/index.js
+++ config/index.js
@@ -9,7 +9,13 @@ var config = {
   DSF_URL: process.env.DSF_URL || 'http://ws-test.infotorg.no/xml/ErgoGroup/DetSentraleFolkeregister1_4/2015-08-10/DetSentraleFolkeregister1_4.wsdl',
   DSF_NAMESPACE: process.env.DSF_NAMESPACE || 'http://ws.infotorg.no/xml/Admin/Brukersesjon/2006-07-07/Brukersesjon.xsd',
   DSF_USERNAME: process.env.DSF_USERNAME || 'MrSmith',
-  DSF_PASSWORD: process.env.DSF_PASSWORD || 'MrSmithsPassword'
+  DSF_PASSWORD: process.env.DSF_PASSWORD || 'MrSmithsPassword',
+  SVARUT_URL: process.env.SVARUT_URL || 'test.svarut.ks.no/tjenester/forsendelseservice/ForsendelsesServiceV4',
+  SVARUT_USERNAME: process.env.SVARUT_USERNAME || 'MrSmith',
+  SVARUT_PASSWORD: process.env.SVARUT_PASSWORD || 'MrSmithsPassword',
+  P360WS_BASEURL: process.env.P360WS_BASEURL || 'http://tfk-fh-siweb01.login.top.no:8088/SI.WS.Core/SIF/',
+  P360WS_USER: process.env.P360WS_USER || 'domain/username',
+  P360WS_PASSWORD: process.env.P360WS_PASSWORD || 'password'
 }
 
 module.exports = config
--- index.js
+++ index.js
@@ -5,12 +5,15 @@ function roy (item, callback) {
   var streamifier = require('streamifier')
   var getNextJob = require('./lib/get-next-job')
   var lookupDsf = require('./lib/lookup-dsf')
+  var lookup360 = require('./lib/lookup-360')
   var saveJobDone = require('./lib/save-job-done')
   var saveJobError = require('./lib/save-job-error')
   var cleanupJob = require('./lib/cleanup-job')
   var cleanupDocuments = require('./lib/cleanup-documents')
   var sendStatusMessage = require('./lib/send-status-message')
+  var sendDocumentsToSvarUt = require('./lib/send-documents-to-svarut')
   var setupItem = require('./lib/setup-item')
+  var setupSvarut = require('./lib/setup-svarut')
   var start = streamifier.createReadStream(JSON.stringify(item))
 
   function finished (error) {
@@ -26,6 +29,9 @@ function roy (item, callback) {
     getNextJob,
     setupItem,
     lookupDsf,
+    lookup360,
+    setupSvarut,
+    sendDocumentsToSvarUt,
     saveJobDone,
     saveJobError,
     cleanupJob,
