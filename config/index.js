22c561c711bcbe0ee0396aec4f786a707861aa1b
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-02-10 10:20:28 +0100

    Adds filter for jobs. Fixes some paths for ENV. Removes .distribution

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@
 var config = {
   ARCHIVE_DIRECTORY_PATH: process.env.ARCHIVE_DIRECTORY_PATH || 'test/data/archive',
   DONE_DIRECTORY_PATH: process.env.DONE_DIRECTORY_PATH || 'test/data/done',
-  ERROR_DIRECTORY_PATH: process.env.DONE_DIRECTORY_PATH || 'test/data/errors',
+  ERROR_DIRECTORY_PATH: process.env.ERROR_DIRECTORY_PATH || 'test/data/errors',
   JOB_DIRECTORY_PATH: process.env.JOB_DIRECTORY_PATH || 'test/data/jobs',
   JWT_KEY: process.env.JWT_KEY || 'Louie Louie, oh no, I got to go',
   DSF_URL: process.env.DSF_URL || 'http://ws-test.infotorg.no/xml/ErgoGroup/DetSentraleFolkeregister1_4/2015-08-10/DetSentraleFolkeregister1_4.wsdl',
