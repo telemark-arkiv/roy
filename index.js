cd9605e82cf945592b272a49d974422086753276
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-02-16 14:57:34 +0100

    Complete rewrite

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ function roy (item, callback) {
   var streamifier = require('streamifier')
   var getNextJob = require('./lib/get-next-job')
   var filterParentsInformation = require('./lib/filter-parents-information')
+  var encodeDocumentsToArchive = require('./lib/encode-documents-to-archive')
   var lookupDsf = require('./lib/lookup-dsf')
   var lookup360 = require('./lib/lookup-360')
   var lookupGuardianInformation = require('./lib/lookup-guardian-information')
   var lookupRestrictedAddress = require('./lib/lookup-restricted-address')
+  var saveJobArchive = require('./lib/save-job-archive')
   var saveJobDone = require('./lib/save-job-done')
   var saveJobError = require('./lib/save-job-error')
   var cleanupJob = require('./lib/cleanup-job')
@@ -17,6 +19,7 @@ function roy (item, callback) {
   var sendDocumentsToSvarUt = require('./lib/send-documents-to-svarut')
   var setupItem = require('./lib/setup-item')
   var setupSvarut = require('./lib/setup-svarut')
+  var setupArchive = require('./lib/setup-archive')
   var unwrapContactInformation = require('./lib/unwrap-contact-information')
   var unwrapParentsInformation = require('./lib/unwrap-parents-information')
   var start = streamifier.createReadStream(JSON.stringify(item))
@@ -42,6 +45,9 @@ function roy (item, callback) {
     lookupRestrictedAddress,
     setupSvarut,
     sendDocumentsToSvarUt,
+    setupArchive,
+    encodeDocumentsToArchive,
+    saveJobArchive,
     saveJobDone,
     saveJobError,
     cleanupJob,
