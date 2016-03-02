4a7e4eccab49c2870264f1e52da2e21d4a7b902d
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-03-02 12:16:14 +0100

    Implements Docker. Namespaces ENV

--- README.md
+++ README.md
@@ -8,3 +8,17 @@ Din mann for levering av dokumenter
 
 - [Beskrivelse](docs/description.md)
 - [Regler](docs/rules.md)
+
+## Docker
+Build
+
+```sh
+$ docker build -t tfk-roy .
+```
+
+### Usage
+```sh
+$ docker run --env-file=docker.env --volume=/test/data/jobs:/src/test/data/jobs --rm tfk-roy
+```
+
+This will start a container. Do the job. Stop the container and remove it.
--- config/index.js
+++ config/index.js
@@ -1,22 +1,22 @@
 'use strict'
 
 var config = {
-  ARCHIVE_DIRECTORY_PATH: process.env.ARCHIVE_DIRECTORY_PATH || 'test/data/archive',
-  DONE_DIRECTORY_PATH: process.env.DONE_DIRECTORY_PATH || 'test/data/done',
-  ERROR_DIRECTORY_PATH: process.env.ERROR_DIRECTORY_PATH || 'test/data/errors',
-  JOB_DIRECTORY_PATH: process.env.JOB_DIRECTORY_PATH || 'test/data/jobs',
-  JWT_KEY: process.env.JWT_KEY || 'Louie Louie, oh no, I got to go',
-  DSF_URL: process.env.DSF_URL || 'http://ws-test.infotorg.no/xml/ErgoGroup/DetSentraleFolkeregister1_4/2015-08-10/DetSentraleFolkeregister1_4.wsdl',
-  DSF_NAMESPACE: process.env.DSF_NAMESPACE || 'http://ws.infotorg.no/xml/Admin/Brukersesjon/2006-07-07/Brukersesjon.xsd',
-  DSF_USERNAME: process.env.DSF_USERNAME || 'MrSmith',
-  DSF_PASSWORD: process.env.DSF_PASSWORD || 'MrSmithsPassword',
-  SVARUT_URL: process.env.SVARUT_URL || 'test.svarut.ks.no/tjenester/forsendelseservice/ForsendelsesServiceV4',
-  SVARUT_USERNAME: process.env.SVARUT_USERNAME || 'MrSmith',
-  SVARUT_PASSWORD: process.env.SVARUT_PASSWORD || 'MrSmithsPassword',
-  SVARUT_KONTERINGSKODE: process.env.SVARUT_KONTERINGSKODE || '1111',
-  P360WS_BASEURL: process.env.P360WS_BASEURL || 'http://tfk-fh-siweb01.login.top.no:8088/SI.WS.Core/SIF/',
-  P360WS_USER: process.env.P360WS_USER || 'domain/username',
-  P360WS_PASSWORD: process.env.P360WS_PASSWORD || 'password'
+  ARCHIVE_DIRECTORY_PATH: process.env.TFK_ROY_ARCHIVE_DIRECTORY_PATH || 'test/data/archive',
+  DONE_DIRECTORY_PATH: process.env.TFK_ROY_DONE_DIRECTORY_PATH || 'test/data/done',
+  ERROR_DIRECTORY_PATH: process.env.TFK_ROY_ERROR_DIRECTORY_PATH || 'test/data/errors',
+  JOB_DIRECTORY_PATH: process.env.TFK_ROY_JOB_DIRECTORY_PATH || 'test/data/jobs',
+  JWT_KEY: process.env.TFK_ROY_JWT_KEY || 'Louie Louie, oh no, I got to go',
+  DSF_URL: process.env.TFK_ROY_DSF_URL || 'http://ws-test.infotorg.no/xml/ErgoGroup/DetSentraleFolkeregister1_4/2015-08-10/DetSentraleFolkeregister1_4.wsdl',
+  DSF_NAMESPACE: process.env.TFK_ROY_DSF_NAMESPACE || 'http://ws.infotorg.no/xml/Admin/Brukersesjon/2006-07-07/Brukersesjon.xsd',
+  DSF_USERNAME: process.env.TFK_ROY_DSF_USERNAME || 'MrSmith',
+  DSF_PASSWORD: process.env.TFK_ROY_DSF_PASSWORD || 'MrSmithsPassword',
+  SVARUT_URL: process.env.TFK_ROY_SVARUT_URL || 'test.svarut.ks.no/tjenester/forsendelseservice/ForsendelsesServiceV4',
+  SVARUT_USERNAME: process.env.TFK_ROY_SVARUT_USERNAME || 'MrSmith',
+  SVARUT_PASSWORD: process.env.TFK_ROY_SVARUT_PASSWORD || 'MrSmithsPassword',
+  SVARUT_KONTERINGSKODE: process.env.TFK_ROY_SVARUT_KONTERINGSKODE || '1111',
+  P360WS_BASEURL: process.env.TFK_ROY_P360WS_BASEURL || 'http://tfk-fh-siweb01.login.top.no:8088/SI.WS.Core/SIF/',
+  P360WS_USER: process.env.TFK_ROY_P360WS_USER || 'domain/username',
+  P360WS_PASSWORD: process.env.TFK_ROY_P360WS_PASSWORD || 'password'
 }
 
 module.exports = config
