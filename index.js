fd2d28946bb0a1209216ce56ece65bec09c1efcd
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-06-28 09:55:10 +0200

    Updates to new standards

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
 'use strict'
 
-function roy (item, callback) {
-  var miss = require('mississippi')
-  var streamifier = require('streamifier')
-  var getNextJob = require('./lib/get-next-job')
-  var filterParentsInformation = require('./lib/filter-parents-information')
-  var encodeDocumentsToArchive = require('./lib/encode-documents-to-archive')
-  var lookupDsf = require('./lib/lookup-dsf')
-  var lookup360 = require('./lib/lookup-360')
-  var lookupGuardianInformation = require('./lib/lookup-guardian-information')
-  var lookupRestrictedAddress = require('./lib/lookup-restricted-address')
-  var saveJobArchive = require('./lib/save-job-archive')
-  var saveJobDone = require('./lib/save-job-done')
-  var saveJobError = require('./lib/save-job-error')
-  var cleanupJob = require('./lib/cleanup-job')
-  var cleanupDocuments = require('./lib/cleanup-documents')
-  var sendStatusMessage = require('./lib/send-status-message')
-  var sendDocumentsToSvarUt = require('./lib/send-documents-to-svarut')
-  var setupItem = require('./lib/setup-item')
-  var setupSvarut = require('./lib/setup-svarut')
-  var setupArchive = require('./lib/setup-archive')
-  var unwrapContactInformation = require('./lib/unwrap-contact-information')
-  var unwrapParentsInformation = require('./lib/unwrap-parents-information')
-  var start = streamifier.createReadStream(JSON.stringify(item))
+module.exports = function roy (item, callback) {
+  const miss = require('mississippi')
+  const getNextJob = require('./lib/get-next-job')
+  const filterParentsInformation = require('./lib/filter-parents-information')
+  const encodeDocumentsToArchive = require('./lib/encode-documents-to-archive')
+  const lookupDsf = require('./lib/lookup-dsf')
+  const lookup360 = require('./lib/lookup-360')
+  const lookupGuardianInformation = require('./lib/lookup-guardian-information')
+  const lookupRestrictedAddress = require('./lib/lookup-restricted-address')
+  const saveJobArchive = require('./lib/save-job-archive')
+  const saveJobDone = require('./lib/save-job-done')
+  const saveJobError = require('./lib/save-job-error')
+  const cleanupJob = require('./lib/cleanup-job')
+  const cleanupDocuments = require('./lib/cleanup-documents')
+  const sendStatusMessage = require('./lib/send-status-message')
+  const sendDocumentsToSvarUt = require('./lib/send-documents-to-svarut')
+  const setupItem = require('./lib/setup-item')
+  const setupSvarut = require('./lib/setup-svarut')
+  const setupArchive = require('./lib/setup-archive')
+  const unwrapContactInformation = require('./lib/unwrap-contact-information')
+  const unwrapParentsInformation = require('./lib/unwrap-parents-information')
+  const start = fromString(JSON.stringify(item))
+
+  function fromString (string) {
+    return miss.from(function (size, next) {
+      // if there's no more content
+      // left in the string, close the stream.
+      if (string.length <= 0) return next(null, null)
+
+      // Pull in a new chunk of text,
+      // removing it from the string.
+      var chunk = string.slice(0, size)
+      string = string.slice(size)
+
+      // Emit "chunk" from the stream.
+      next(null, chunk)
+    })
+  }
 
   function finished (error) {
     if (error) {
@@ -56,5 +71,3 @@ function roy (item, callback) {
     finished
   )
 }
-
-module.exports = roy
