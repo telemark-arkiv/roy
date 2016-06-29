22fb1233c8094f17cb0034ec16f5be742af857d1
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-06-29 12:16:59 +0200

    Drops looking for guardians if no necessary

--- config/index.js
+++ config/index.js
@@ -10,10 +10,13 @@ var config = {
   DSF_NAMESPACE: process.env.TFK_ROY_DSF_NAMESPACE || 'http://ws.infotorg.no/xml/Admin/Brukersesjon/2006-07-07/Brukersesjon.xsd',
   DSF_USERNAME: process.env.TFK_ROY_DSF_USERNAME || 'MrSmith',
   DSF_PASSWORD: process.env.TFK_ROY_DSF_PASSWORD || 'MrSmithsPassword',
+  DSF_METHOD: process.env.TFK_ROY_DSF_METHOD || 'hentForeldre',
+  DSF_SAKSREF: process.env.TFK_ROY_DSF_SAKSREF || 'MinElev',
   SVARUT_URL: process.env.TFK_ROY_SVARUT_URL || 'test.svarut.ks.no/tjenester/forsendelseservice/ForsendelsesServiceV4',
   SVARUT_USERNAME: process.env.TFK_ROY_SVARUT_USERNAME || 'MrSmith',
   SVARUT_PASSWORD: process.env.TFK_ROY_SVARUT_PASSWORD || 'MrSmithsPassword',
   SVARUT_KONTERINGSKODE: process.env.TFK_ROY_SVARUT_KONTERINGSKODE || '1111',
+  SVARUT_AVGIVENDE_SYSTEM: process.env.SVARUT_AVGIVENDE_SYSTEM || 'MinElev',
   P360WS_BASEURL: process.env.TFK_ROY_P360WS_BASEURL || 'http://tfk-fh-siweb01.login.top.no:8088/SI.WS.Core/SIF/',
   P360WS_USER: process.env.TFK_ROY_P360WS_USER || 'domain/username',
   P360WS_PASSWORD: process.env.TFK_ROY_P360WS_PASSWORD || 'password'
